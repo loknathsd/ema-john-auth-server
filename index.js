e7c7cc9a15cb3643bb8510694e7cb19690e13098
unknown <loknathsds66@gmail.com>
2021-11-26 20:34:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vfsjf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors())
+
+
+const port = 4000
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const products = client.db("emaStore").collection("product");
+  
+    app.post('/addProduct', (req , res)=>{
+      const product = req.body;
+      console.log(product)
+      products.insertOne(product)
+      .then(result =>{
+        console.log(result)
+      })
+    })
+
+
+  client.close();
+});
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
